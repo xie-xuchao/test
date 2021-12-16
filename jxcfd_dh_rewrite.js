cfc45d05208423178eb89bd80970afb029d1fa1a
xie-xuchao <77599712+xie-xuchao@users.noreply.github.com>
2021-12-16 11:58:14 +0800

    Update jxcfd_dh_rewrite.js

--- jxcfd_dh_rewrite.js
+++ jxcfd_dh_rewrite.js
@@ -1,9 +1,10 @@
-const path = "\/jxbfd\/user\/ExchangeState";
 
 let obj = JSON.parse($response.body);
-$notify("test", "test", obj);
-if( $request.url.indexOf(path) != -1){
+$notify("test", "test", "test");
+/*
+if(obj){
 	obj.data.hongbao[0]["dwState"] = "0";
 	}
 	
 $done({body:JSON.stringify(obj)});
+*/
