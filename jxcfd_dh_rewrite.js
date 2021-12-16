703c94418d4cd8714022e9259f6ff07f7c1d868d
xie-xuchao <77599712+xie-xuchao@users.noreply.github.com>
2021-12-16 12:06:06 +0800

    Update jxcfd_dh_rewrite.js

--- jxcfd_dh_rewrite.js
+++ jxcfd_dh_rewrite.js
@@ -1,10 +1,8 @@
 
 let obj = JSON.parse($response.body);
 $notify("test", "test", "test");
-/*
 if(obj){
-	obj.data.hongbao[0]["dwState"] = "0";
+	obj.hongbao[0]["dwState"] = "0";
 	}
 	
 $done({body:JSON.stringify(obj)});
-*/
